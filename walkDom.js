3f955f27f89f899ee3ce685aa47a8a0c28d3dce8
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-07-31 18:20:45 +0200

    Update walkDom.js

--- walkDom.js
+++ walkDom.js
@@ -1,25 +1,40 @@
-function use(depth,childIndex,node){
- console.log('-'.repeat(depth),childIndex,node.tagName);
+const path=[];
+//
+//parentHasChildren
+// if(e.parentElement.children.length>1){s+='?'};
+ //console.log('-'.repeat(depth),childIndex,n.tagName);
+
+function use(node){
+ const m=path, e=node,j=e.childIndex;
+/**/
+ let s=e.tagName;
+ if(j!==1&&s!=='BODY'){
+	s=s+':nth-child('+j+')';
+ };
+ m[(m.length=e.depth)]=[s];
+ console.log(m.join('\u0020>\u0020'));
+
 }
 //===========================
 function walkDom(process){
  const d=document;
  let e=d.children[0];
  if(e){
-	const f=process, o=d.createTreeWalker((e=d.createDocumentFragment().appendChild(e.cloneNode(true))),1,null,false);
+	const f=process,o=d.createTreeWalker((e=d.createDocumentFragment().appendChild(e.cloneNode(true))),1,null,false);
 	e.depth=0;
 	do{
-	 let n,i,j;
+	 let n,i=0,j=1;
 	 if((n=e.previousElementSibling)!==null){
 		i=n.depth;
 		j=n.childIndex+1;
 	 }else{
-		i=(n=e.parentElement)!==null?n.depth+1:0;
-		j=1;
+		if((n=e.parentElement)!==null){
+		 i=n.depth+1;
+		};
 	 };
 	 e.depth=i;
 	 e.childIndex=j;
-	 f(i,j,e);
+	 f(e);
 	}while((e=o.nextNode())!==null);
  };
 }
